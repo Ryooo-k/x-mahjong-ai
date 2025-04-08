71ea14124daeeae4671ab2f73a2ff557d8b79fa7
ryo <ta090972@icloud.com>
2025-04-08 16:49:27 +0900

    playerクラスとテストを作成

---
+++ src/environment/player.rb
@@ -0,0 +1,166 @@
+# frozen_string_literal: true
+
+class Player
+  attr_reader :score, :point_histories, :hand_histories, :called_tile_table, :rivers
+
+  def initialize(id)
+    @id = id
+    reset
+  end
+
+  def reset
+    @score = 25_000
+    @point_histories = []
+    reset_hand_state
+    self
+  end
+
+  def restart
+    reset_hand_state
+  end
+
+  def hands
+    {
+      tiles: @hands,
+      ids: @hands.map(&:id),
+      suits: @hands.map(&:suit),
+      numbers: @hands.map(&:number),
+      codes: @hands.map(&:code),
+      names: @hands.map(&:name)
+    }
+  end
+
+  def sorted_hands
+    sorted_hands = @hands.sort_by(&:id)
+
+    {
+      tiles: sorted_hands,
+      ids: sorted_hands.map(&:id),
+      suits: sorted_hands.map(&:suit),
+      numbers: sorted_hands.map(&:number),
+      codes: sorted_hands.map(&:code),
+      names: sorted_hands.map(&:name)
+    }
+  end
+
+  def add_point(point)
+    @score += point
+  end
+
+  def record_point(point)
+    @point_histories << point
+  end
+
+  def draw(tile)
+    tile.holder = self
+    @hands << tile
+  end
+
+  def play(tile)
+    raise ArgumentError, '手牌に無い牌は選択できません。' unless @hands.include?(tile)
+
+    @hands.delete(tile)
+    @rivers << tile
+    @hand_histories << @hands.dup
+  end
+
+  def pong(combinations, target_tile)
+    raise ArgumentError, '有効な牌が無いためポンできません。' unless can_call_pong?(target_tile)
+    preform_call(combinations, target_tile:)
+  end
+
+  def chow(combinations, target_tile)
+    raise ArgumentError, '有効な牌が無いためチーできません。' unless can_call_chow?(target_tile)
+    preform_call(combinations, target_tile:)
+  end
+
+  def concealed_kong(combinations)
+    raise ArgumentError, '有効な牌が無いため暗カンできません。' unless can_call_concealed_kong?(combinations)
+    preform_call(combinations)
+  end
+
+  def open_kong(combinations, target_tile)
+    raise ArgumentError, '有効な牌が無いため大明カンできません。' unless can_call_open_kong?(target_tile)
+    preform_call(combinations, target_tile:)
+  end
+
+  def extended_kong(target_tile)
+    raise ArgumentError, '有効な牌が無いため加カンできません。' unless can_call_extended_kong?(target_tile)
+    
+    @called_tile_table.each do |called_tiles|
+      called_codes = called_tiles.map(&:code)
+      called_tiles << target_tile if called_codes.uniq.size == 1 && called_codes.first == target_tile.code
+    end
+  end
+
+  # def ron(target)
+  # end
+
+  private
+
+  def reset_hand_state
+    @hands = []
+    @hand_histories = []
+    @called_tile_table = []
+    @rivers = []
+  end
+
+  def can_call_pong?(target)
+    hand_codes = @hands.map(&:code)
+    hand_codes.count(target.code) >= 2
+  end
+
+  def can_call_chow?(target)
+    return false if target.code >= 31 # 字牌はチーできないので早期return
+
+    hand_codes = @hands.map(&:code)
+    possible_chow_table = build_possible_chow_table(target)
+    possible_chow_table.any? do |possible_chow_codes|
+      possible_chow_codes.all? { |code| hand_codes.include?(code) }
+    end
+  end
+
+  def can_call_concealed_kong?(combinations)
+    target_codes = combinations.map(&:code)
+    return false if combinations.size != 4 || target_codes.uniq.size != 1
+
+    target_code = target_codes.first
+    hand_codes = @hands.map(&:code)
+    hand_codes.count(target_code) == 4
+  end
+
+  def can_call_open_kong?(target)
+    hand_codes = @hands.map(&:code)
+    hand_codes.count(target.code) == 3
+  end
+
+  def can_call_extended_kong?(target)
+    pong_code_table = @called_tile_table.map do |called_tiles|
+      called_codes = called_tiles.map(&:code)
+      called_codes.uniq.size == 1 ? called_codes : next
+    end
+
+    pong_code_table.any?{ |pong_codes| pong_codes.count(target.code) == 3 }
+  end
+
+  def preform_call(combinations, target_tile: false)
+    called_tiles = combinations.dup
+    target_tile.holder = self if target_tile
+    called_tiles << target_tile if target_tile
+
+    @called_tile_table << called_tiles
+    called_tiles.each { |tile| @hands.delete(tile) }
+    @hand_histories << @hands.dup
+  end
+
+  def build_possible_chow_table(target)
+    n = target.number
+    code = target.code
+  
+    candidates = []
+    candidates << [code + 1, code + 2] if n <= 7
+    candidates << [code - 1, code + 1] if (2..8).include?(n)
+    candidates << [code - 2, code - 1] if n >= 3
+    candidates
+  end
+end
---
+++ test/player_test.rb
@@ -0,0 +1,235 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative '../src/environment/player'
+require_relative '../src/environment/tile'
+
+class TileTest < Test::Unit::TestCase
+  def setup
+    @player = Player.new(0)
+    @other_player = Player.new(1)
+    @manzu_1_tile = Tile.new(0)
+    @manzu_2_tile = Tile.new(4)
+    @manzu_3_tile_id8 = Tile.new(8)
+    @manzu_3_tile_id9 = Tile.new(9)
+    @manzu_3_tile_id10 = Tile.new(10)
+    @manzu_3_tile_id11 = Tile.new(11)
+    @east_tile = Tile.new(108)
+  end
+
+  def test_initialize_player
+    expected = {
+      score: 25_000,
+      point_histories: [],
+      hands: { tiles: [], ids: [], suits: [], numbers: [], codes: [], names: []},
+      hand_histories: [],
+      called_tile_table: [],
+      rivers: []
+    }
+
+    assert_equal(expected[:score], @player.score)
+    assert_equal(expected[:point_histories], @player.point_histories)
+    assert_equal(expected[:hands], @player.hands)
+    assert_equal(expected[:hand_histories], @player.hand_histories)
+    assert_equal(expected[:called_tile_table], @player.called_tile_table)
+    assert_equal(expected[:rivers], @player.rivers)
+  end
+
+  def test_add_tile_to_hands_when_player_drew_tile
+    @player.draw(@manzu_1_tile)
+    assert_equal(@manzu_1_tile, @player.hands[:tiles].first)
+    assert_equal(@manzu_1_tile.id, @player.hands[:ids].first)
+    assert_equal(@manzu_1_tile.suit, @player.hands[:suits].first)
+    assert_equal(@manzu_1_tile.number, @player.hands[:numbers].first)
+    assert_equal(@manzu_1_tile.code, @player.hands[:codes].first)
+    assert_equal(@manzu_1_tile.name, @player.hands[:names].first)
+  end
+
+  def test_holder_is_set_when_player_drew_tile
+    @player.draw(@manzu_1_tile)
+    assert_equal(@player, @manzu_1_tile.holder)
+  end
+
+  def test_hands_return_hand_state
+    @player.draw(@manzu_1_tile)
+    @player.draw(@manzu_2_tile)
+    expected = {
+      tiles: [@manzu_1_tile, @manzu_2_tile],
+      ids: [@manzu_1_tile.id, @manzu_2_tile.id],
+      suits: [@manzu_1_tile.suit, @manzu_2_tile.suit],
+      numbers: [@manzu_1_tile.number, @manzu_2_tile.number],
+      codes: [@manzu_1_tile.code, @manzu_2_tile.code],
+      names: [@manzu_1_tile.name, @manzu_2_tile.name]
+    }
+    assert_equal(expected, @player.hands)
+  end
+
+  def test_sorted_hands_return_hands_sorted_by_id
+    @player.draw(@east_tile)
+    @player.draw(@manzu_2_tile)
+    @player.draw(@manzu_1_tile)
+    assert_equal(['東', '2萬','1萬'], @player.hands[:names])
+    assert_equal(['1萬', '2萬', '東'], @player.sorted_hands[:names])
+  end
+
+  def test_score_increase_with_added_point
+    assert_equal(25_000, @player.score)
+  
+    @player.add_point(8_000)
+    assert_equal(33_000, @player.score)
+  
+    @player.add_point(-12_000)
+    assert_equal(21_000, @player.score)
+  end
+
+  def test_add_point_to_history_on_score_increase
+    @player.record_point(8_000)
+    assert_equal([8_000], @player.point_histories)
+
+    @player.record_point(-12_000)
+    assert_equal([8_000, -12_000], @player.point_histories)
+  end
+
+  # playメソッドのテスト
+  def test_remove_tile_from_hand_when_player_played_tile
+    @player.draw(@manzu_1_tile)
+    @player.draw(@manzu_2_tile)
+
+    @player.play(@manzu_1_tile)
+    assert_equal([@manzu_2_tile], @player.hands[:tiles])
+  end
+
+  def test_add_tile_from_river_when_player_played_tile
+    assert_equal([], @player.rivers)
+    @player.draw(@manzu_1_tile)
+    @player.play(@manzu_1_tile)
+    assert_equal([@manzu_1_tile], @player.rivers)
+  end
+
+  def test_add_tile_from_hand_histories_when_player_played_tile
+    @player.draw(@manzu_1_tile)
+    @player.draw(@manzu_2_tile)
+    @player.draw(@east_tile)
+    @player.play(@east_tile)
+    assert_equal([[@manzu_1_tile, @manzu_2_tile]], @player.hand_histories)
+
+    @player.play(@manzu_1_tile)
+    assert_equal([[@manzu_1_tile, @manzu_2_tile], [@manzu_2_tile]], @player.hand_histories)
+  end
+
+  def test_can_not_play_when_tile_not_in_hand
+    error = assert_raise(ArgumentError) { @player.play(@manzu_1_tile) }
+    assert_equal('手牌に無い牌は選択できません。', error.message)
+  end
+
+  # pongメソッドのテスト
+  def test_tile_holder_change_to_pong_player
+    @other_player.draw(@manzu_3_tile_id8)
+    assert_equal(@other_player, @manzu_3_tile_id8.holder)
+
+    combinations = [@manzu_3_tile_id9, @manzu_3_tile_id10]
+    @player.draw(@manzu_3_tile_id9)
+    @player.draw(@manzu_3_tile_id10)
+    @player.pong(combinations, @manzu_3_tile_id8)
+    assert_equal(@player, @manzu_3_tile_id8.holder)
+  end
+
+  def test_hands_delete_target_tiles_when_player_called_pong
+    target = @manzu_3_tile_id8
+    combinations = [@manzu_3_tile_id9, @manzu_3_tile_id10]
+    @player.draw(@manzu_1_tile)
+    @player.draw(@manzu_3_tile_id9)
+    @player.draw(@manzu_3_tile_id10)
+    @player.pong(combinations, target)
+
+    called_tile_table = combinations << target
+    @player.hands[:tiles].each { |tile| assert_not_include(called_tile_table, tile) }
+  end
+
+  def test_called_tile_table_add_target_tiles_when_player_called_pong
+    target = @manzu_3_tile_id8
+    combinations = [@manzu_3_tile_id9, @manzu_3_tile_id10]
+    @player.draw(@manzu_3_tile_id9)
+    @player.draw(@manzu_3_tile_id10)
+    @player.pong(combinations, target)
+
+    called_tile_table = combinations << target
+    assert_equal([called_tile_table], @player.called_tile_table)
+  end
+
+  def test_can_not_call_pong_when_combinations_not_in_hand
+    combinations = [@manzu_3_tile_id9, @manzu_3_tile_id10]
+    error = assert_raise(ArgumentError) { @player.pong(combinations, @manzu_3_tile_id8) }
+    assert_equal('有効な牌が無いためポンできません。', error.message)
+  end
+
+  # chowメソッドのテスト
+  def test_can_not_call_chow_when_combinations_not_in_hand
+    combinations = [@manzu_1_tile, @manzu_2_tile]
+    error = assert_raise(ArgumentError) { @player.chow(combinations, @manzu_3_tile_id8) }
+    assert_equal('有効な牌が無いためチーできません。', error.message)
+  end
+
+  # concealed_kongメソッドのテスト
+  def test_hands_delete_target_tiles_when_player_called_concealed_kong
+    @player.draw(@manzu_3_tile_id8)
+    @player.draw(@manzu_3_tile_id9)
+    @player.draw(@manzu_3_tile_id10)
+    @player.draw(@manzu_3_tile_id11)
+    @player.draw(@east_tile)
+    combinations = [@manzu_3_tile_id8, @manzu_3_tile_id9, @manzu_3_tile_id10, @manzu_3_tile_id11]
+    @player.concealed_kong(combinations)
+    @player.hands[:tiles].each { |tile| assert_not_include(combinations, tile) }
+  end
+
+  def test_called_tile_table_add_target_tiles_when_player_called_concealed_kong
+    @player.draw(@manzu_3_tile_id8)
+    @player.draw(@manzu_3_tile_id9)
+    @player.draw(@manzu_3_tile_id10)
+    @player.draw(@manzu_3_tile_id11)
+    @player.draw(@east_tile)
+    combinations = [@manzu_3_tile_id8, @manzu_3_tile_id9, @manzu_3_tile_id10, @manzu_3_tile_id11]
+    @player.concealed_kong(combinations)
+    assert_equal([combinations], @player.called_tile_table)
+  end
+
+  def test_can_not_call_concealed_kong_when_combinations_not_in_hand
+    combinations = [@manzu_3_tile_id8, @manzu_3_tile_id9, @manzu_3_tile_id10, @manzu_3_tile_id11]
+    error = assert_raise(ArgumentError) { @player.concealed_kong(combinations) }
+    assert_equal('有効な牌が無いため暗カンできません。', error.message)
+  end
+
+  # open_kongメソッドのテスト
+  def test_can_not_call_open_kong_when_combinations_not_in_hand
+    target = @manzu_3_tile_id11
+    combinations = [@manzu_3_tile_id8, @manzu_3_tile_id9, @manzu_3_tile_id10]
+    error = assert_raise(ArgumentError) { @player.open_kong(combinations, target) }
+    assert_equal('有効な牌が無いため大明カンできません。', error.message)
+  end
+
+  # extended_kongメソッドのテスト
+  def test_called_tile_table_add_target_tile_when_player_called_extended_kong
+    combinations = [@manzu_3_tile_id8, @manzu_3_tile_id9]
+    @player.draw(@manzu_3_tile_id8)
+    @player.draw(@manzu_3_tile_id9)
+    @player.pong(combinations, @manzu_3_tile_id10)
+    @player.extended_kong(@manzu_3_tile_id11)
+
+    expected = [[@manzu_3_tile_id8, @manzu_3_tile_id9, @manzu_3_tile_id10, @manzu_3_tile_id11]]
+    assert_equal(expected, @player.called_tile_table)
+  end
+
+  def test_can_not_call_extended_kong_when_no_existing_pong
+    error = assert_raise(ArgumentError) { @player.extended_kong(@manzu_3_tile_id8) }
+    assert_equal('有効な牌が無いため加カンできません。', error.message)
+  end
+
+  # def test_reset
+  #   point = 8_000
+  #   tiles = Array.new(13) { |id| Tile.new(id, id / 4, false) }
+  #   @player.add_score(point)
+  #   tiles.each { |tile| @player.draw(tile) } 
+  
+  # end
+
+end

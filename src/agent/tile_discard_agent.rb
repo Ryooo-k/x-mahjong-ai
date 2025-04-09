807667b89ffd332938a3ab76d5db952545d590a4
ryo <ta090972@icloud.com>
2025-04-09 23:01:59 +0900

    手牌のうち捨てる牌を選択するエージェントを作成

---
+++ src/agent/tile_discard_agent.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'torch'
+require 'numo/narray'
+require_relative 'qnet'
+require_relative '../util/replay_buffer'
+
+class TileDiscardAgent
+  def initialize(gamma:, lr:, epsilon:, buffer_size:, batch_size:, min_epsilon:, decay_rate:, layer_config:)
+    @gamma = gamma
+    @lr = lr
+    @epsilon = epsilon
+    @buffer_size = buffer_size
+    @batch_size = batch_size
+    @action_size = 14
+    @min_epsilon = min_epsilon
+    @decay_rate = decay_rate
+    @device = Torch.device(Torch::Backends::MPS.available? ? "mps" : "cpu")
+    @replay_buffer = ReplayBuffer.new(@buffer_size, @batch_size, @device)
+    @q_net = QNet.new(layer_config, @action_size).to(@device)
+    @q_net_target = QNet.new(layer_config, @action_size).to(@device)
+    @criterion = Torch::NN::MSELoss.new #平均２乗誤差
+    @optimizer = Torch::Optim::Adam.new(@q_net.parameters, lr: @lr)
+  end
+
+  def get_action(states)
+    if rand < @epsilon
+      rand(@action_size)
+    else
+      tensor_states = Torch.tensor(states, dtype: :float32).unsqueeze(0).to(@device)
+      qualities = @q_net.call(tensor_states).detach
+      qualities.numo.argmax
+    end
+  end
+
+  def update(state, action, reward, next_state, done)
+    @replay_buffer.add(state.cpu, action, reward, next_state.cpu, done)
+    return Torch.tensor(0) if @replay_buffer.buffers.size < @batch_size
+
+    states, actions, rewards, next_states, donee = @replay_buffer.get_batch
+    batch_number = Torch.arange(@batch_size, dtype: :long)
+    qualities = @q_net.call(states)
+    action_qualities = qualities[batch_number, actions]
+
+    next_qualities = @q_net_target.call(next_states)
+    next_max_qualities = next_qualities.max(1)[0].detach
+    targets = rewards + (1 - donee) * @gamma * next_max_qualities
+    loss = @criterion.call(action_qualities, targets)
+
+    @optimizer.zero_grad
+    loss.backward
+    @optimizer.step
+    loss.data
+  end
+
+  def sync_qnet
+    @q_net_target.load_state_dict(@q_net.state_dict)
+  end
+
+  def update_epsilon
+    @epsilon = [@min_epsilon, @epsilon * @decay_rate].max
+  end
+end
---
+++ test/agent/tile_discard_agent_test.rb
@@ -0,0 +1,143 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative '../../src/agent/tile_discard_agent'
+
+class TileDiscardAgentTest < Test::Unit::TestCase
+  class DummyBuffer
+    attr_reader :buffers
+
+    def initialize
+      @buffers = []
+    end
+
+    def add(*); end
+
+    def get_batch
+      batch_size = 2
+      [
+        Torch.randn([batch_size, 38]),      # states
+        Torch.tensor([0, 1], dtype: :long), # actions
+        Torch.tensor([1.0, 0.0]),           # rewards
+        Torch.randn([batch_size, 38]),      # next_states
+        Torch.tensor([0.0, 1.0])            # donee
+      ]
+    end
+  end
+
+  class DummyQNet
+    def call(x)
+      Torch.tensor([[1.0, 2.0], [3.0, 0.5]])  # argmax_index = 2
+    end
+  end
+
+  class DummyCriterion
+    def call(qualities, targets)
+      Torch.tensor(999.0, requires_grad: true)
+    end
+  end
+
+  class DummyOptimizer
+    def zero_grad; end
+    def step; end
+  end
+
+  def setup
+    layer_config = { layer1: 38, layer2: 64 }
+    @agent = TileDiscardAgent.new(
+      gamma: 0.98,
+      lr: 0.0001,
+      epsilon: 0.0,
+      buffer_size: 2,
+      batch_size: 2,
+      min_epsilon: 0.01,
+      decay_rate: 0.99999,
+      layer_config:
+    )
+
+    @agent.instance_variable_set(:@replay_buffer, DummyBuffer.new)
+    @agent.instance_variable_set(:@q_net, DummyQNet.new)
+    @agent.instance_variable_set(:@q_net_target, DummyQNet.new)
+    @agent.instance_variable_set(:@criterion, DummyCriterion.new)
+    @agent.instance_variable_set(:@optimizer, DummyOptimizer.new)
+  end
+
+  def test_get_action_return_argmax_when_epsilon_is_zero
+    input = [0.1] * 38
+    action = @agent.get_action(input)
+    assert_equal 2, action  # 期待値: mock_qnetのargmax_index の 2 を期待する
+  end
+
+  def test_get_action_return_random_when_epsilon_is_high
+    @agent.instance_variable_set(:@epsilon, 1.0)  # 必ず探索を行う設定
+    input = [0.1] * 38
+    action = @agent.get_action(input)
+    action_size = 14
+    assert_includes (0...action_size).to_a, action
+  end
+
+  def test_update_return_zero_when_buffer_is_small
+    dummy_state = Torch.tensor(1.0)
+    loss = @agent.update(dummy_state, nil, nil, dummy_state, nil)
+    assert_equal 0, loss.item
+  end
+
+  def test_update_return_loss_tensor
+    dummy_state = Torch.tensor(1.0)
+    buffer = DummyBuffer.new
+    buffer.buffers << [] << []  # バッチサイズと同じ数を入れておく
+    @agent.instance_variable_set(:@replay_buffer, buffer)
+    loss = @agent.update(dummy_state, nil, nil, dummy_state, nil)
+    assert_instance_of(Torch::Tensor, loss)
+    assert_equal(999.0, loss.item) # DummyCriterionのcallメソッドの戻り値と一致していること
+  end
+
+  def test_sync_qnet_copies_weights
+    layer_config = { layer1: 38, layer: 68 }
+    action_size = 14
+    qnet1 = QNet.new(layer_config, action_size)
+    qnet2 = QNet.new(layer_config, action_size)
+    refute_equal(qnet1.state_dict.to_s, qnet2.state_dict.to_s)  
+
+    qnet2.load_state_dict(qnet1.state_dict)
+    assert_equal(qnet1.state_dict.to_s, qnet2.state_dict.to_s)
+  end
+
+  def test_update_epsilon_decreases_properly
+    layer_config = { layer1: 38, layer: 68 }
+    epsilon = 1.0
+    decay_rate = 0.9
+    agent = TileDiscardAgent.new(
+      gamma: 0.9,
+      lr: 0.001,
+      epsilon:,
+      buffer_size: 1,
+      batch_size: 1,
+      min_epsilon: 0.1,
+      decay_rate:,
+      layer_config:
+    )
+
+    agent.update_epsilon
+    expected = epsilon * decay_rate
+    assert_equal(expected, agent.instance_variable_get(:@epsilon))
+  end
+
+  def test_update_epsilon_does_not_go_below_min
+    layer_config = { layer1: 38, layer: 68 }
+    min_epsilon = 0.5
+    agent = TileDiscardAgent.new(
+      gamma: 0.9,
+      lr: 0.001,
+      epsilon: 0.01,
+      buffer_size: 1,
+      batch_size: 1,
+      min_epsilon:,
+      decay_rate: 0.1,
+      layer_config:
+    )
+  
+    agent.update_epsilon
+    assert_equal(min_epsilon, agent.instance_variable_get(:@epsilon))
+  end  
+end

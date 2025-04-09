e7cd521313e3075a6ef5f7aacbc16d7e056240f4
ryo <ta090972@icloud.com>
2025-04-09 16:32:08 +0900

    シャンテンリストを取得するモジュールを作成

--- src/util/shanten_list_loader.rb
+++ src/util/shanten_list_loader.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-# require 'json'
+require 'json'
 
-# module Utils
-#   module ShantenLoader
-#     def self.load(path = 'data/shanten_list.json')
-#       JSON.parse(File.read(path))
-#     end
-#   end
-# end
+module ShantenLoader
+  def self.load(path = 'data/shanten_list.json')
+    JSON.parse(File.read(path))
+  end
+end

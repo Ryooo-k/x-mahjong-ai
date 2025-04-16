c2e9897e80063e85ec2f105d296cac73cf1b936c
ryo <ta090972@icloud.com>
2025-04-16 14:17:30 +0900

    loaderモジュールを拡張した

---
+++ src/util/file_loader.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'yaml'
+
+module FileLoader
+  def self.load_shanten_list
+    path = File.expand_path('../data/shanten_list.json', __dir__)
+    JSON.parse(File.read(path))
+  end
+
+  def self.load_parameter(target)
+    path = File.expand_path('../../config/parameter.yml', __dir__)
+    config = YAML.load_file(path)
+    config[target]
+  end
+end
--- src/util/shanten_list_loader.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'json'
-
-module ShantenLoader
-  def self.load(path = 'data/shanten_list.json')
-    JSON.parse(File.read(path))
-  end
-end

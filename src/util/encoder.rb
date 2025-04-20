eba2158e77854e7b8ebb12e13d031a44dafe88e3
ryo <ta090972@icloud.com>
2025-04-21 00:14:01 +0900

    encoderのテストを追加

--- src/util/encoder.rb
+++ src/util/encoder.rb
@@ -7,13 +7,13 @@ module Util
     MAX_RIVER_COUNT = 24
     MAX_DORA_COUNT = 5
 
-    def encode_hands(hands)
+    def self.encode_hands(hands)
       codes = Array.new(TILE_COUNT, 0)
       hands.each { |tile| codes[tile.code] += 1 }
       codes
     end
 
-    def encode_called_tile_table(called_tile_table)
+    def self.encode_called_tile_table(called_tile_table)
       code_table = Array.new(MAX_CALL_COUNT) { Array.new(TILE_COUNT, 0) }
       called_tile_table.each_with_index do |called_tiles, order|
         called_tiles.each { |tile| code_table[order][tile.code] += 1 }
@@ -21,18 +21,20 @@ module Util
       code_table
     end
 
-    def encode_rivers(rivers)
+    def self.encode_rivers(rivers)
       normalized_river_codes = Array.new(MAX_RIVER_COUNT, -1)
       rivers.each_with_index do |tile, order|
         normalized_river_codes[order] = tile.code / TILE_COUNT
       end
+      normalized_river_codes
     end
 
-    def encode_dora(dora_tiles)
+    def self.encode_dora(dora_tiles)
       normalized_dora_codes = Array.new(MAX_DORA_COUNT, -1)
       dora_tiles.each_with_index do |tile, order|
         normalized_dora_codes[order] = tile.code / TILE_COUNT
       end
+      normalized_dora_codes
     end
   end
 end
---
+++ test/util/encoder_test.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative '../../src/domain/tile'
+require_relative '../../src/util/encoder'
+
+class EncoderTest < Test::Unit::TestCase
+  def setup
+    @encoder = Util::Encoder
+    @manzu_1 = Tile.new(0)
+    @manzu_2 = Tile.new(4)
+    @manzu_3 = Tile.new(8)
+    @manzu_4 = Tile.new(12)
+    @manzu_5 = Tile.new(16)
+    @manzu_6 = Tile.new(20)
+    @tile_count = 34.0
+  end
+
+  def test_encode_hands
+    hands = []
+    encoded_hands = @encoder.encode_hands(hands)
+    expected = [0] * 34
+    assert_equal(expected, encoded_hands)
+
+    hands = [@manzu_1, @manzu_2, @manzu_2, @manzu_3, @manzu_3, @manzu_3]
+    encoded_hands = @encoder.encode_hands(hands)
+    expected = [1, 2, 3] + [0] * 31
+    assert_equal(expected, encoded_hands)
+  end
+
+  def test_encode_called_tile_table
+    called_tile_table = []
+    encoded_called_tile_table = @encoder.encode_called_tile_table(called_tile_table)
+    expected = [
+      [0] * 34,
+      [0] * 34,
+      [0] * 34,
+      [0] * 34
+    ]
+    assert_equal(expected, encoded_called_tile_table)
+
+    called_tile_table = [[@manzu_1, @manzu_2, @manzu_3], [@manzu_4, @manzu_5, @manzu_6]]
+    encoded_called_tile_table = @encoder.encode_called_tile_table(called_tile_table)
+    expected = [
+      [1, 1, 1] + [0] * 31,
+      [0, 0, 0, 1, 1, 1] + [0] * 28,
+      [0] * 34,
+      [0] * 34
+    ]
+    assert_equal(expected, encoded_called_tile_table)
+  end
+
+  def test_encode_rivers
+    rivers = []
+    encoded_rivers = @encoder.encode_rivers(rivers)
+    expected = [-1] * 24
+    assert_equal(expected, encoded_rivers)
+
+    rivers = [@manzu_6, @manzu_1]
+    encoded_rivers = @encoder.encode_rivers(rivers)
+    normalized_manzu_6_code = @manzu_6.code / @tile_count
+    normalized_manzu_1_code = @manzu_1.code / @tile_count
+    expected = [normalized_manzu_6_code, normalized_manzu_1_code ] + [-1] * 22
+    assert_equal(expected, encoded_rivers)
+  end
+
+  def test_encode_dora
+    dora_tiles = []
+    encoded_dora_tiles = @encoder.encode_dora(dora_tiles)
+    expected = [-1] * 5
+    assert_equal(expected, encoded_dora_tiles)
+
+    dora_tiles = [@manzu_1, @manzu_2]
+    encoded_dora_tiles = @encoder.encode_dora(dora_tiles)
+    normalized_manzu_1_code = @manzu_1.code / @tile_count
+    normalized_manzu_2_code = @manzu_2.code / @tile_count
+    expected = [normalized_manzu_1_code , normalized_manzu_2_code] + [-1] * 3
+    assert_equal(expected, encoded_dora_tiles)
+  end
+end

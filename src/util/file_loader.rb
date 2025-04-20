63a3b7180f716cda8e6ea13c523c27695a6fd404
ryo <ta090972@icloud.com>
2025-04-20 16:30:59 +0900

    名前空間を追加

--- src/util/file_loader.rb
+++ src/util/file_loader.rb
@@ -3,15 +3,17 @@
 require 'json'
 require 'yaml'
 
-module FileLoader
-  def self.load_shanten_list
-    path = File.expand_path('../data/shanten_list.json', __dir__)
-    JSON.parse(File.read(path))
-  end
+module Util
+  module FileLoader
+    def self.load_shanten_list
+      path = File.expand_path('../data/shanten_list.json', __dir__)
+      JSON.parse(File.read(path))
+    end
 
-  def self.load_parameter(target)
-    path = File.expand_path('../../config/parameter.yml', __dir__)
-    config = YAML.load_file(path)
-    config[target]
+    def self.load_parameter(target)
+      path = File.expand_path('../../config/parameter.yml', __dir__)
+      config = YAML.load_file(path)
+      config[target]
+    end
   end
 end

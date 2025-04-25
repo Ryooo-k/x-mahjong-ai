7f593f848ab48a264c0e1c8faeef5716a17cd9a7
ryo <ta090972@icloud.com>
2025-04-25 13:42:20 +0900

    envからtableを取得できるようにした

--- test/util/state_builder_test.rb
+++ test/util/state_builder_test.rb
@@ -10,20 +10,19 @@ class StateBuilderTest < Test::Unit::TestCase
     @builder = Util::StateBuilder
     parameter = FileLoader.load_parameter
     @env = Env.new(parameter['table'], parameter['player'])
-    @table = Table.new(parameter['table'], parameter['player'])
   end
 
   def test_build_states_return_697_values
     current_player = @env.current_player
     other_players = @env.other_players
-    states = @builder.build_states(current_player, other_players, @table)
+    states = @builder.build_states(current_player, other_players, @env.table)
     assert_equal 697, states.length
   end
 
   def test_build_states_return_tensor_data
     current_player = @env.current_player
     other_players = @env.other_players
-    states = @builder.build_states(current_player, other_players, @table)
+    states = @builder.build_states(current_player, other_players, @env.table)
     assert_equal Torch::Tensor, states.class
   end
 end

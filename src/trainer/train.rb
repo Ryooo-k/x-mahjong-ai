ad7fb2884868c472a758455011d2e52de5d49e21
ryo <ta090972@icloud.com>
2025-04-25 16:15:13 +0900

    経過時間の出力を変更

--- src/trainer/train.rb
+++ src/trainer/train.rb
@@ -7,33 +7,43 @@ require_relative '../environment/env'
 def run_training
   config = Util::FileLoader.load_parameter('experiment_1')
   env = Env.new(config['table'], config['player'])
-  second = Benchmark.realtime { run_training_loop(config['train'], env) }
-  puts format_second(second)
+  time_taken = Benchmark.realtime { run_training_loop(config['train'], env) }
+  puts format_second(time_taken)
 end
 
 def run_training_loop(train_config, env)
+  total_time = 0
   train_config['count'].times do |count|
-    done = false
-    env.reset
+    time_taken = Benchmark.realtime do |_|
+      done = false
+      env.reset
+      time = 0
 
-    while not done
-      current_player = env.current_player
-      env.player_draw
-      states = env.states
-      action = current_player.get_discard_action(states)
-      next_states, reward, done, discard_tile = env.step(action)
-      current_player.update_discard_agent(states, action, reward, next_states, done)
-      env.rotate_turn
+      while not done
+        current_player = env.current_player
+        env.player_draw
+        states = env.states
+        action = current_player.get_discard_action(states)
+        next_states, reward, done, discard_tile = env.step(action)
+        current_player.update_discard_agent(states, action, reward, next_states, done)
+        env.rotate_turn
+      end
+
+      env.sync_qnet_for_all_players if count % train_config['qnet_sync_interval'] == 0
     end
-
-    env.sync_qnet_for_all_players if count % train_config['qnet_sync_interval'] == 0
-    puts "学習回数：#{count}" if count % 100 == 0
-    puts env.log_training_info if count % 100 == 0
+    total_time += time_taken
+    output(total_time, count, env.log_training_info) if count % 10 == 0
   end
 end
 
-def format_second(second)
-  hours = second / 3600
-  minutes = (second % 3600) / 60
-  "#{hours.to_i}時間 #{minutes.to_i}分 #{second.to_i}秒"
+def output(time_taken, count, info)
+  puts format_second(time_taken)
+  puts "学習回数：#{count}"
+  puts info
+end
+
+def format_second(time_taken)
+  hours = time_taken / 3600
+  minutes = (time_taken % 3600) / 60
+  "#{hours.to_i}時間 #{minutes.to_i}分"
 end

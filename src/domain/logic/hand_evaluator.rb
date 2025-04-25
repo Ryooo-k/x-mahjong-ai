0f5d8c801cd0b928e7511eed4dfa8497250d249b
ryo <ta090972@icloud.com>
2025-04-26 00:11:54 +0900

    hand_evaluatorを最適化

--- src/domain/logic/hand_evaluator.rb
+++ src/domain/logic/hand_evaluator.rb
@@ -8,10 +8,12 @@ require_relative '../../util/encoder'
 module Domain
   module Logic
     module HandEvaluator
-      ALL_TILES = (0..135).map { |id| Tile.new(id) }.freeze
-      KOKUSHI_TILE_CODES = [0, 8, 9, 17, 18, 26, 27, 28, 29, 30, 31, 32, 33]
+      ALL_TILES = Array.new(136) { |id| Tile.new(id) }.freeze
+      TILE_MAP = ALL_TILES.group_by(&:code).transform_values(&:freeze).freeze
+      KOKUSHI_TILE_CODES = [0, 8, 9, 17, 18, 26, 27, 28, 29, 30, 31, 32, 33].to_set.freeze
       KOKUSHI_TILES = ALL_TILES.select { |tile| KOKUSHI_TILE_CODES.include?(tile.code) }
       SHANTEN_LIST = Util::FileLoader.load_shanten_list
+      CHIITOITSU_PAIR_COUNT = 7
 
       def self.count_minimum_outs(hands)
         normal_outs = count_normal_outs(hands)
@@ -59,31 +61,44 @@ module Domain
 
       def self.count_normal_outs(hands)
         current_shanten = calculate_minimum_shanten(hands)
+        hand_ids = hands.map(&:id).to_set
+        hand_codes = hands.map(&:code)
+        code_counts = hand_codes.tally
+
         ALL_TILES.select do |tile|
-          next if hands.map(&:id).include?(tile.id)
-          test_hands = hands.dup << tile
-          test_codes = test_hands.map(&:code)
-          next unless test_codes.tally.all? { |_, tile_count| tile_count < 5 }
-          test_shanten = calculate_minimum_shanten(test_hands)
+          next if hand_ids.include?(tile.id)
+          code = tile.code
+          next if (code_counts[code] || 0) >= 4
+
+          test_shanten = calculate_minimum_shanten(hands + [tile])
           test_shanten < current_shanten
         end
       end
 
       def self.count_chiitoitsu_outs(hands)
-        outs_tile_codes = hands.map(&:code).tally.select { |_, count| count == 1 }.keys
-        ALL_TILES.select do |tile|
-          next if hands.map(&:id).include?(tile.id)
-          outs_tile_codes.include?(tile.code)
+        hand_ids = hands.map(&:id).to_set
+        code_counts = hands.map(&:code).tally
+        single_codes = code_counts.select { |_, count| count == 1 }.keys
+
+        single_codes.flat_map do |code|
+          TILE_MAP[code].reject { |tile| hand_ids.include?(tile.id) }
         end
       end
 
       def self.count_kokushi_outs(hands)
-        used_kokushi_codes = hands.map(&:code).select { |tile_code| KOKUSHI_TILE_CODES.include?(tile_code) }
-        unused_kokushi_codes = KOKUSHI_TILE_CODES.select { |code| !used_kokushi_codes.include?(code) }
+        hand_codes = hands.map(&:code)
+        hand_ids = hands.map(&:id).to_set
+
+        used_kokushi_codes = hand_codes.select { |code| KOKUSHI_TILE_CODES.include?(code) }
+        used_code_tally = used_kokushi_codes.tally
         is_head = used_kokushi_codes.tally.values.any? { |count| count >= 2 }
-        is_head ?
-          KOKUSHI_TILES.select { |tile| unused_kokushi_codes.include?(tile.code) } :
-          KOKUSHI_TILES.select { |tile| !hands.map(&:id).include?(tile.id) }
+
+        if is_head
+          unused_code_set = (KOKUSHI_TILE_CODES - used_kokushi_codes).to_set
+          KOKUSHI_TILES.select { |tile| unused_code_set.include?(tile.code) }
+        else
+          KOKUSHI_TILES.reject { |tile| hand_ids.include?(tile.id) }
+        end
       end
 
       def self.calculate_all_shanten(hands)
@@ -94,40 +109,38 @@ module Domain
       end
 
       def self.calculate_normal_shanten(hands)
-        encoded_hands = Util::Encoder.encode_hands(hands)
-        manzu_code = encoded_hands[0..8].to_a.map(&:to_i).to_s
-        pinzu_code = encoded_hands[9..17].to_a.map(&:to_i).to_s
-        souzu_code = encoded_hands[18..26].to_a.map(&:to_i).to_s
-        zihai_code = encoded_hands[27..33].to_a.map(&:to_i).to_s
+        encoded = Util::Encoder.encode_hands(hands)
 
-        min_agari_distance = NORMAL_AGARI_PATTERNS.map do |numbers|
-          manzu_count = numbers[0].to_s
-          pinzu_count = numbers[1].to_s
-          souzu_count = numbers[2].to_s
-          zihai_count = numbers[3].to_s
+        manzu_code  = encoded[0..8].to_s
+        pinzu_code  = encoded[9..17].to_s
+        souzu_code  = encoded[18..26].to_s
+        zihai_code  = encoded[27..33].to_s
 
-          manzu_agari_distance = SHANTEN_LIST['suuhai'][manzu_code][manzu_count]
-          pinzu_agari_distance = SHANTEN_LIST['suuhai'][pinzu_code][pinzu_count]
-          souzu_agari_distance = SHANTEN_LIST['suuhai'][souzu_code][souzu_count]
-          zihai_agari_distance = SHANTEN_LIST['zihai'][zihai_code][zihai_count]
-          manzu_agari_distance + pinzu_agari_distance + souzu_agari_distance + zihai_agari_distance
-        end.min
-        min_agari_distance - 1
+        min_distance = Float::INFINITY
+        NORMAL_AGARI_PATTERNS.each do |manzu_n, pinzu_n, souzu_n, zihai_n|
+          sum =
+            SHANTEN_LIST['suuhai'][manzu_code][manzu_n.to_s] +
+            SHANTEN_LIST['suuhai'][pinzu_code][pinzu_n.to_s] +
+            SHANTEN_LIST['suuhai'][souzu_code][souzu_n.to_s] +
+            SHANTEN_LIST['zihai'][zihai_code][zihai_n.to_s]
+          min_distance = sum if sum < min_distance
+        end
+        min_distance - 1
       end
-  
+
       def self.calculate_chiitoitsu_shanten(hands)
-        hand_codes = hands.map(&:code)
-        chiitoitsu_match_count = hand_codes.tally.values.select { |count| count >= 2 }.size
-        agari_distance = 7 - chiitoitsu_match_count
-        agari_distance - 1
+        code_counts = hands.map(&:code).tally
+        pair_count = 0
+        code_counts.each_value { |count| pair_count += 1 if count >= 2 }
+        CHIITOITSU_PAIR_COUNT - pair_count - 1
       end
-  
+
       def self.calculate_kokushi_shanten(hands)
-        hand_codes = hands.map(&:code)
-        used_kokushi_codes = hand_codes.select { |code| KOKUSHI_TILE_CODES.include?(code) }
-        unused_kokushi_codes = used_kokushi_codes.uniq
-        is_head = used_kokushi_codes.tally.values.any? { |count| count >= 2 }
-        13 - unused_kokushi_codes.size - (is_head ? 1 : 0)
+        code_counts = hands.map(&:code).tally
+        used_kokushi_codes = code_counts.select { |code, _| KOKUSHI_TILE_CODES.include?(code) }
+        unique_count = used_kokushi_codes.keys.size
+        has_head = used_kokushi_codes.values.any? { |count| count >= 2 }
+        13 - unique_count - (has_head ? 1 : 0)
       end
     end
   end

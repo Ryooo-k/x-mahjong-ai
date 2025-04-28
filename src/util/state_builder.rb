589d4f9b64f51474c21466e48e68955b6d6ae65e
ryo <ta090972@icloud.com>
2025-04-28 14:09:36 +0900

    手配をソートして出力

--- src/util/state_builder.rb
+++ src/util/state_builder.rb
@@ -21,14 +21,16 @@ module Util
       def build_log_training_info(table)
         info = ["ツモ回数：#{table.draw_count}"]
         table.players.each do |player|
-          start_hand = player.hand_histories.first
-          start_hand_shanten = Domain::Logic::HandEvaluator.calculate_minimum_shanten(start_hand)
-          end_hand = player.hand_histories.last
-          end_hand_shanten = Domain::Logic::HandEvaluator.calculate_minimum_shanten(end_hand)
+          start_hands = player.hand_histories.first.sort_by(&:id)
+          start_hand_shanten = Domain::Logic::HandEvaluator.calculate_minimum_shanten(start_hands)
+          end_hands = player.hand_histories.last.sort_by(&:id)
+          end_hand_shanten = Domain::Logic::HandEvaluator.calculate_minimum_shanten(end_hands)
+          start_hand_names = start_hands.map(&:name).join(' ')
+          end_hand_names = end_hands.map(&:name).join(' ')
 
           info << "プレイヤー#{player.id}"
-          info << "配　　牌：#{start_hand.map(&:name).sort.join(' ')} 、向聴数：#{start_hand_shanten}"
-          info << "最終手牌：#{end_hand.map(&:name).sort.join(' ')} 、向聴数：#{end_hand_shanten}"
+          info << "配　　牌：#{start_hand_names} 、向聴数：#{start_hand_shanten}"
+          info << "最終手牌：#{end_hand_names} 、向聴数：#{end_hand_shanten}"
           info << ''
         end
         info << '-' * 50

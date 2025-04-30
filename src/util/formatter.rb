536fa10b83d4f7b1a3c9a5377b84984e51263c4a
ryo <ta090972@icloud.com>
2025-04-30 13:58:30 +0900

    学習ログの出力を調整

--- src/util/formatter.rb
+++ src/util/formatter.rb
@@ -1,25 +1,36 @@
 # frozen_string_literal: true
 
+require_relative '../domain/logic/hand_evaluator'
+
 module Util
   module Formatter
+
+    HandEvaluator = Domain::Logic::HandEvaluator
+
     class << self
       def build_training_log(table)
-        info = ["ツモ回数：#{table.draw_count}"]
-        table.players.each do |player|
+        log = ["ツモ回数：#{table.draw_count}\n"]
+
+        table.seat_orders.each do |player|
           start_hands = player.hand_histories.first.sort_by(&:id)
           start_hand_shanten = HandEvaluator.calculate_minimum_shanten(start_hands)
           end_hands = player.hand_histories.last.sort_by(&:id)
           end_hand_shanten = HandEvaluator.calculate_minimum_shanten(end_hands)
           start_hand_names = start_hands.map(&:name).join(' ')
           end_hand_names = end_hands.map(&:name).join(' ')
+          shanten_histories = player.shanten_histories.join(' -> ')
+          outs_histories = player.outs_histories.join(' -> ')
 
-          info << "プレイヤー#{player.id}"
-          info << "配　　牌：#{start_hand_names} 、向聴数：#{start_hand_shanten}"
-          info << "最終手牌：#{end_hand_names} 、向聴数：#{end_hand_shanten}"
-          info << ''
+          log << "プレイヤー#{player.id}"
+          log << "配　　牌：#{start_hand_names} 、向聴数：#{start_hand_shanten}"
+          log << "最終手牌：#{end_hand_names} 、向聴数：#{end_hand_shanten}"
+          log << "向聴数の推移：#{shanten_histories}"
+          log << "アウツの推移：#{outs_histories}"
+          log << "loss：#{player.agent.total_discard_loss}"
+          log << ''
         end
-        info << '-' * 50
-        info.join("\n")
+        log << '-' * 100
+        log.join("\n")
       end
 
       def convert_number_to_kanji(num)

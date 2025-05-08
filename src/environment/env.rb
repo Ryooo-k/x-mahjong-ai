61c95c3f9b252dc80008942e0bf200d534350c32
ryo <ta090972@icloud.com>
2025-05-08 22:48:54 +0900

    envクラスを通常麻雀に拡張 - ラウンドの移行とゲーム終了のフラグとなる変数を保持 - step関数でツモ和了、ロン和了、和了無しの場合の分岐を設定 - 連チャン、次局移行、ゲームリセットの場合の初期化関数を追加 - testは未修正

--- src/environment/env.rb
+++ src/environment/env.rb
@@ -7,26 +7,22 @@ require_relative '../util/formatter'
 
 class Env
   attr_reader :table, :current_player, :other_players
+  attr_accessor :game_over, :round_over
 
   Formatter = Util::Formatter
   STARTING_HAND_COUNT = 13
 
   def initialize(table_config, player_config)
     @table = Table.new(table_config, player_config)
-    deal_starting_hand
-    @done = false
+    @game_over = false
+    @round_over = false
     @current_player = @table.host
     @other_players = @table.children
-  end
-
-  def reset
-    @table.reset
     deal_starting_hand
-    @done = false
+    set_player_wind
   end
 
   def player_draw
-    return if game_over?
     top_tile = @table.top_tile
     @current_player.draw(top_tile)
     @table.increase_draw_count
@@ -36,23 +32,17 @@ class Env
     StateBuilder.build_states(@current_player, @other_players, @table)
   end
 
-  def step(action)
-    old_shanten = @current_player.shanten_histories.last
-    old_outs = @current_player.outs_histories.last
+  def step(discard_action)
+    return handle_agari if @current_player.agari?
 
-    is_agari = @current_player.agari?
-    target_tile = @current_player.choose(action) unless is_agari
-    @current_player.discard(target_tile) unless is_agari
+    target_tile = @current_player.choose(discard_action)
+    @current_player.discard(target_tile)
     @current_player.record_hand_status
 
-    new_hands = @current_player.hands
-    new_shanten = @current_player.shanten_histories.last
-    new_outs = @current_player.outs_histories.last
+    ron_action = get_ron_action(target_tile)
+    return handle_ron(target_tile) if ron_action
 
-    reward = cal_reward(old_shanten, new_shanten, old_outs, new_outs, is_agari)
-    @done = true if is_agari || game_over?
-
-    [states, reward, @done, target_tile]
+    handle_normal_progress(target_tile)
   end
 
   def rotate_turn
@@ -63,12 +53,37 @@ class Env
     @other_players = rotated_orders[1..]
   end
 
+  def update_agent
+  end
+
   def sync_qnet_for_all_players
     @table.players.each { |player| player.sync_qnet }
   end
 
   def log
     Formatter.build_training_log(@table)
+  end
+
+  def renchan?
+    @table.host.agari?
+  end
+
+  def restart
+    @table.restart
+    prepare_round
+  end
+
+  def proceed_to_next_round
+    @table.proceed_to_next_round
+    prepare_round
+    set_player_wind
+  end
+
+  def reset
+    @table.reset
+    @game_over = false
+    prepare_round
+    set_player_wind
   end
 
   private
@@ -84,13 +99,57 @@ class Env
     end
   end
 
-  def game_over?
+  def prepare_round
+    @round_over = false
+    @current_player = @table.host
+    @other_players = @table.children
+    deal_starting_hand
+  end
+
+  def set_player_wind
+    @table.wind_orders.each_with_index { |player, i| player.wind = "#{i + 1}z" }
+  end
+
+  def round_over?
     @table.draw_count + @table.kong_count >= 122
+  end
+
+  def get_ron_action(tile)
+    ron_action = nil
+    round_wind = @table.round[:wind]
+
+    @other_players.each_with_index do |player, order|
+      ron_action = player.get_ron_action(states) if player.can_ron?(tile, round_wind)
+      break if !ron_action.nil?
+    end
+    ron_action
+  end
+
+  def handle_agari
+    reward = 100
+    @round_over = true
+    target_tile = nil
+    [states, reward, @round_over, target_tile]
+  end
+
+  def handle_ron(target_tile)
+    reward = 100
+    @round_over = true
+    target_tile = nil
+    [states, reward, @round_over, target_tile]
+  end
+
+  def handle_normal_progress(target_tile)
+    old_shanten, old_outs = get_previous_status
+    new_shanten, new_outs = get_current_status
+    reward = cal_reward(old_shanten, new_shanten, old_outs, new_outs, false)
+    @round_over = round_over?
+    [states, reward, @round_over, target_tile]
   end
 
   def cal_reward(old_shanten, new_shanten, old_outs, new_outs, is_agari)
     return 100 if is_agari
-    return -100 if game_over?
+    return -100 if round_over?
 
     diff_shanten = new_shanten - old_shanten
     diff_outs = new_outs - old_outs
@@ -105,5 +164,4 @@ class Env
 
     -50
   end
-
 end

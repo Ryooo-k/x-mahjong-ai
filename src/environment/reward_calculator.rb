10f7e8369e51e382cd6f0663a7c4912787852d6d
ryo <ta090972@icloud.com>
2025-05-21 14:30:29 +0900

    ツモ、打牌時のrewardを算出するメソッドを作成

--- src/environment/reward_calculator.rb
+++ src/environment/reward_calculator.rb
@@ -12,20 +12,30 @@ module RewardCalculator
   NORMALIZATION_BASE_POINT = 10_000.0
 
   class << self
-    def calculate_normal_reward(player, round_over)
-      round_over ? calculate_round_over_reward(player) : calculate_round_continue_reward(player)
+    def calculate_tsumo_rewards(current_player, other_players, round_over)
+      all_players = [current_player] + other_players
+      round_over ? calculate_round_over_rewards(all_players) : [0] * 4
     end
 
-    def calculate_agari_reward(player, round_over)
-      round_over ? calculate_round_over_reward(player) : 0
+    def calculate_discard_rewards(current_player, other_players, round_over)
+      all_players = [current_player] + other_players
+
+      if round_over
+        calculate_round_over_rewards(all_players)
+      else
+        reward = calculate_round_continue_reward(current_player)
+        [reward, 0, 0, 0]
+      end
     end
 
     private
 
-    def calculate_round_over_reward(player)
-      rank_reward = REWARDS_BY_RANK[player.rank]
-      score_reward = player.score / NORMALIZATION_BASE_POINT
-      rank_reward + score_reward
+    def calculate_round_over_rewards(players)
+      players.map do |player|
+        rank_reward = REWARDS_BY_RANK[player.rank]
+        score_reward = player.score / NORMALIZATION_BASE_POINT
+        rank_reward + score_reward
+      end
     end
 
     def calculate_round_continue_reward(player)
@@ -37,8 +47,8 @@ module RewardCalculator
       old_outs = player.outs_histories[-2]
       diff_outs = current_outs - old_outs
 
-      return 1 if diff_shanten < 0
-      return 1 if current_shanten == 0 # 聴牌維持の場合、報酬を1
+      return 2 if diff_shanten < 0
+      return 2 if current_shanten == 0 # 聴牌維持の場合、報酬を1
       return 1 if diff_shanten == 0 && diff_outs > 0
       return 0 if diff_shanten == 0 && diff_outs == 0
 
